4ac8ab44a078e3819e73074a0c74119932e1566d
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-25 23:03:19 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,27 @@
 class ProductsController < ApplicationController
-
-  def description
-    productd = Product.find(params[:id])
-    render plain: product.description
+  def new
+    @product = Product.new
   end
 
-  def inventory
-    productd = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
+  def create
+    @product = Product.create(params.require(:product).permit(:name, :description, :inventory))
+    redirect_to products_path
+  end
+
+  def show
   end
 
   def index
     @products = Product.all
   end
 
-  def create
-    Product.create(product_params)
-    redirect_to products_path
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
   end
 
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :description, :inventory, :price)
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
 
-  resources :prodcuts, only: [:index, :new, :create]
+  get '/products/:id/inventory', to: 'products#inventory'
   get '/products/:id/description', to: 'products#description'
-  get '/products/:id/invoice', to: 'products#invoice'
+
 end
