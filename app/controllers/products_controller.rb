b16dc8c3785f217e10fa3b3564463523e68ae4cc
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-25 23:05:27 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,22 +9,22 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     render plain: product.description
   end
-  
-  def index 
-    @products = Product.all 
-  end 
-  
-  def create 
-    Product.create(product_params) 
-    redirect_to products_path 
-  end 
-  
-  
+
+  def index
+    @products = Product.all
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+
   private
-  
-  def product_params 
+
+  def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
-  end 
-  
+  end
+
 
 end
